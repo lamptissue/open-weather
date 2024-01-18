c91414008e920f580095060e82deaeb5f38c3493
lamptissue <ncarribine@hotmail.co.uk>
2024-01-18 16:22:02 +0000

    Added styling + testing for netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import { useEffect, useState, useRef } from "react";
+import { useState } from "react";
 export default function Home() {
   const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
   const [weatherData, setWeatherData] = useState(null);
-  const [selectCity, setSelectCity] = useState("");
+  const [selectCity, setSelectCity] = useState("london");
   const [unitTemperature, setUnitTemperature] = useState("metric");
   const [errorMessage, setErrorMessage] = useState("");
 
@@ -17,7 +17,7 @@ export default function Home() {
         setWeatherData(data);
       } else {
         setWeatherData(null);
-        setErrorMessage("not a valid location");
+        setErrorMessage("Not a valid location");
       }
     } catch (error) {
       console.error("Error:", error);
@@ -56,66 +56,100 @@ export default function Home() {
       newData.unshift(filterTodayWeather);
     }
   }
-  console.log("new", newData);
-  return (
-    <main>
-      <div>
-        <input type='text' name='weatherLocation' id='location' placeholder='Location' onChange={handleInputChange} />
-        <input
-          type='radio'
-          id='celsius'
-          name='temperature'
-          value='metric'
-          checked={unitTemperature === "metric"}
-          onChange={handleUnitChange}
-        />
-        <label htmlFor='celsius'>°C</label>
-        <input
-          type='radio'
-          id='fahrenheit'
-          name='temperature'
-          value='imperial'
-          checked={unitTemperature === "imperial"}
-          onChange={handleUnitChange}
-        />
-        <label htmlFor='fahrenheit'>°F</label>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={handleSubmit}>
-          Submit
-        </button>
-      </div>
-      {errorMessage && <p className='error-message'>{errorMessage}</p>}
 
-      {weatherData && (
-        <>
-          <h1>
-            5 day weather for {weatherData.city.name}, {weatherData.city.country}
-          </h1>
-          <div className='testContainer'>
-            {newData.map((item) => {
-              const date = new Date(item.dt * 1000).toDateString();
-              const weather = item.weather[0];
-              return (
-                <div key={item.dt} className='test-box'>
-                  <p>{date}</p>
-                  <img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} />
-                  <p>{weather.description} </p>
-                  <p>
-                    Temperature:
-                    {unitTemperature === "metric"
-                      ? `${item.main.temp.toFixed(0)}°C`
-                      : `${celsiusToFahrenheit(item.main.temp).toFixed(0)}°F`}
-                  </p>
-                  <p>Wind: {item.wind.speed}m/s</p>
-                  {item.pop ? <p>Chance of rain: {item.pop * 100}%</p> : ""}
-                  {item.rain && `Rain: ${item.rain["3h"]} mm`}
-                  <p>Humidity: {item.main.humidity}</p>
-                  <p>Pressure: {item.main.pressure}</p>
-                </div>
-              );
-            })}
+  const Weather = () => {
+    return (
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+        {newData.map((item) => {
+          const date = new Date(item.dt * 1000).toDateString();
+          const weather = item.weather[0];
+
+          return (
+            <div
+              key={item.dt}
+              className='bg-white rounded-lg border-black border shadow-md p-4 flex flex-col items-center text-center'
+            >
+              <p className='font-bold text-lg mb-2'>{date}</p>
+              <img
+                src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`}
+                alt={`${weather.description} icon`}
+                className='mb-2'
+              />
+              <p className='text-sm text-gray-600 mb-1'>{weather.description}</p>
+              <p className='font-semibold'>
+                Temperature:{" "}
+                <span className='text-blue-600'>
+                  {unitTemperature === "metric"
+                    ? `${item.main.temp.toFixed(0)}°C`
+                    : `${celsiusToFahrenheit(item.main.temp).toFixed(0)}°F`}
+                </span>
+              </p>
+              <p className='text-sm text-gray-600'>Wind: {item.wind.speed}m/s</p>
+              {item.pop ? <p className='text-sm text-gray-600'>Chance of rain: {Math.round(item.pop * 100)}%</p> : ""}
+              {item.rain && <p className='text-sm text-gray-600'>Rain: {item.rain["3h"]} mm</p>}
+              <p className='text-sm text-gray-600'>Humidity: {item.main.humidity}%</p>
+              <p className='text-sm text-gray-600'>Pressure: {item.main.pressure} hPa</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  return (
+    <main className='bg-orange-100 min-h-screen flex justify-center items-center'>
+      <div className='container mx-auto p-5 items-center'>
+        <div className='flex items-center mb-6 gap-4'>
+          <div>
+            {weatherData ? (
+              <h1 className='text-3xl font-semibold'>
+                5 day weather for {weatherData.city.name}, {weatherData.city.country}
+              </h1>
+            ) : (
+              <h1>Ya ma</h1>
+            )}
           </div>
-        </>
-      )}
+          <div className='flex items-center bg-slate-500 rounded-full overflow-hidden shadow-md'>
+            <input
+              type='text'
+              name='weatherLocation'
+              id='location'
+              placeholder='Enter a location'
+              className='p-2   rounded-l-full flex-grow'
+              onChange={handleInputChange}
+            />
+            <div className='flex gap-2'>
+              {/* Temperature */}
+              <input
+                type='radio'
+                id='celsius'
+                name='temperature'
+                value='metric'
+                checked={unitTemperature === "metric"}
+                onChange={handleUnitChange}
+              />
+              <label htmlFor='celsius'>°C</label>
+              <input
+                type='radio'
+                id='fahrenheit'
+                name='temperature'
+                value='imperial'
+                checked={unitTemperature === "imperial"}
+                onChange={handleUnitChange}
+              />
+              <label htmlFor='fahrenheit'>°F</label>
+            </div>
+            <button
+              className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-full'
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+        {errorMessage && <p className='error-message'>{errorMessage}</p>}
+        {weatherData && <Weather />}
+      </div>
     </main>
   );
 }
